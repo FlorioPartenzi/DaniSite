bf13d8dfd1f508a438b22914e0f16b1c0b71e0c9
Florio Partenzi <florio@partenzi.de>
2022-11-03 13:42:03 +0100

    feat(vita): added vita section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DeckblattComponent } from './deckblatt/deckblatt.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { VitaComponent } from './vita/vita.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DeckblattComponent
+    DeckblattComponent,
+    NavbarComponent,
+    VitaComponent
   ],
   imports: [
     BrowserModule,
--- src/app/deckblatt/deckblatt.component.ts
+++ src/app/deckblatt/deckblatt.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-deckblatt',
   templateUrl: './deckblatt.component.html',
-  styleUrls: ['./deckblatt.component.scss']
+  styleUrls: ['./deckblatt.component.scss'],
 })
 export class DeckblattComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
